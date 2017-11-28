e67fe28b042d69bed6e26cf0472c0e46226ef15e
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2017-11-28 16:56:57 +0200

    Задание #2#2

--- js/stat.js
+++ js/stat.js
@@ -4,21 +4,10 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'white';
   ctx.shadowOffsetX = 10;
   ctx.shadowOffsetY = 10;
-  ctx.shadowBlur = 10;
+  ctx.shadowBlur = 1;
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
 
-  ctx.beginPath();
-  ctx.moveTo(100, 10);
-  ctx.lineTo(310, 30);
-  ctx.lineTo(520, 10);
-  ctx.lineTo(490, 145);
-  ctx.lineTo(520, 280);
-  ctx.lineTo(310, 260);
-  ctx.lineTo(100, 280);
-  ctx.lineTo(130, 145);
-  ctx.lineTo(100, 10);
-  ctx.closePath();
-  ctx.fill();
+  ctx.fillRect(100, 10, 420, 270);
 
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
@@ -26,13 +15,13 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 220, 40);
-  ctx.fillText('Список результатов:', 220, 70);
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
 
   var maxTime = -1;
   var maxTimeIndex = -1;
   var startX = 150;
-  var startY = 300;
+  var startY = 240;
   var step = 50;
   var barWidth = 40;
   var barHeight = 150;
@@ -47,18 +36,16 @@ window.renderStatistics = function (ctx, names, times) {
 
   var coeffHeight = -150 / (maxTime);
 
-  ctx.fillText('Худшее время: ' + parseInt(maxTime, 10) + 'мс у игрока '
-                + names[maxTimeIndex], 140, 100);
-
   for (var j = 0; j < times.length; j++) {
     if (names[j] !== 'Вы') {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + (0.2 + 0.1 * j) + ')';
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + (0.5 + 0.1 * j) + ')';
     } else {
-      ctx.fillStyle = 'rgba(0, 255, 0, 1)';
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
 
     ctx.fillRect(startX + (barWidth + step) * j, startY, barWidth, times[j] * coeffHeight);
     ctx.fillStyle = 'black';
-    ctx.fillText(names[j], startX + (barWidth + step) * j, barHeight - baseline);
+    ctx.fillText(names[j], startX + (barWidth + step) * j, startY + (baseline + 5));
+    ctx.fillText(parseInt(times[j], 10), startX + (barWidth + step) * j, startY + (times[j] * coeffHeight) - baseline);
   }
 };
